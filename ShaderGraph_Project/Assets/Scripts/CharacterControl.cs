bbd72e6ffb81268431aca27007da2c8e4389d6cd
Thomas Lamb <s200498@students.aie.edu.au>
2021-03-29 20:42:11 +1100

    Added basic movement linked to animations Fix Left right snapping

---
+++ ShaderGraph_Project/Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour
+{
+
+
+    Animator animator;
+    CharacterController controller;
+
+    public float MoveSpeed = 10.0f;
+    private Vector3 velocity;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Animation changing for aiming 
+        if (Input.GetMouseButtonDown(1))
+        {
+            animator.SetBool("IsAiming", true);
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            animator.SetBool("IsAiming", false);
+        }
+
+        float forward = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+        velocity.x = horizontal * MoveSpeed * Time.deltaTime;
+        velocity.z = forward * MoveSpeed * Time.deltaTime;
+
+        animator.SetFloat("Xpos", horizontal);
+        animator.SetFloat("Ypos", forward);
+
+
+        controller.Move(velocity);
+    }
+}
