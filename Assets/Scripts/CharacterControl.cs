6e222901270bcff66061c7dfa7d495f4eaf0bb68
Thomas Lamb <s200498@students.aie.edu.au>
2021-03-31 13:32:21 +1100

    Added camera and animation smoothing

--- Assets/Scripts/CharacterControl.cs
+++ Assets/Scripts/CharacterControl.cs
@@ -12,15 +12,29 @@ public class CharacterControl : MonoBehaviour
     public float MoveSpeed = 10.0f;
     private Vector3 velocity;
 
+    public float rotateSpeedX = 10f;
+    public float rotateSpeedY = 8f;
+
+
+    //this is for animation blending for instant change
+    //so the animations arent jarred the current x and y pos are lerped inbetween to smooth
+    float prevHorizontal = 0;
+    float prevForward = 0;
+    public float animationSwapTime = 1f;
+
     void Start()
     {
         animator = GetComponent<Animator>();
         controller = GetComponent<CharacterController>();
+        rotateSpeedX *= 100f;
+        rotateSpeedY *= 100f;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+
         //Animation changing for aiming 
         if (Input.GetMouseButtonDown(1))
         {
@@ -37,10 +51,22 @@ public class CharacterControl : MonoBehaviour
         velocity.x = horizontal * MoveSpeed * Time.deltaTime;
         velocity.z = forward * MoveSpeed * Time.deltaTime;
 
+        forward = Mathf.Lerp(prevForward, forward, animationSwapTime);
+        horizontal = Mathf.Lerp(prevHorizontal, horizontal, animationSwapTime);
+
         animator.SetFloat("Xpos", horizontal);
         animator.SetFloat("Ypos", forward);
 
+        prevHorizontal = horizontal;
+        prevForward = forward;
 
+        
         controller.Move(velocity);
+        transform.TransformDirectio
+
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        transform.Rotate(new Vector3(0, 1, 0), mouseX * rotateSpeedX * Time.deltaTime);
     }
 }
